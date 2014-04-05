70f447b048be45343b3164da7a5fa805b8fa6667
Ryan Hanks <ryan.hanks@webfilings.com>
2014-04-04 21:51:39 -0700

    update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,30 @@
-mkdir ~/.pip 
-echo "[global]
-timeout = 15
-index-url = https://ryanhanks-wf:e789c9a2a4@pypi.webfilings.org/simple/" > ~/.pip/pip.conf 
+echo "https://github.com/settings/ssh" | pbcopy
+echo "**************************************************"
+echo "1. open https://github.com/settings/ssh (in your pasteboard) and continue"
+
+xcode-select --install
+
 mkdir .ssh
 cd .ssh/
 ssh-keygen -t rsa -C "ryan.hanks@webfilings.com"
 ssh-add ~/.ssh/id_rsa
-cat https://github.com/settings/ssh
-say "open github ssh key page and press ctrl d"
+cd
+
 cat
 pbcopy < ~/.ssh/id_rsa.pub
-cat https://github.com/settings/ssh
-say "add ssh key"
+cat ~/.ssh/id_rsa.pub
+say "add the key"
+echo "**************************************************"
+echo "2. Add the key and press ctrl+d to continue"
 cat
+
 ssh -T git@github.com
-date
-# xcode-select --install
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew doctor
-brew install node cmake python ant jenkins
-pip install virtualenv
- 
- 
-npm install -g grunt-cli
-npm install -g cordova
-npm install -g phantomjs
- 
-git clone git@github.com:ryanhanks-wf/bdub
+git clone git@github.com:ryanhanks-wf/bdub 
+echo "**************************************************"
+echo "press ctrl+d when xcode install completes"
+cat
 
 cd bdub
-
 sudo gem install bundler
 sudo bundle
-
-sudo pmset sleep 90
-
 bundle exec soloist
-
-
