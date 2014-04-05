31720e95f727eb4be6cb810573a486743a6263df
Ryan Hanks <ryan.hanks@webfilings.com>
2014-04-05 09:58:34 -0700

    Fixup bootstrap. Rename brony to brono

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,5 @@
+open http://support.apple.com/downloads/DL1572/en_US/JavaForOSX2013-05.dmg
+
 echo "https://github.com/settings/ssh" | pbcopy
 echo "**************************************************"
 echo "1. open https://github.com/settings/ssh (in your pasteboard) and continue"
@@ -7,11 +9,12 @@ xcode-select --install
 mkdir ~/.ssh
 cd ~/.ssh/
 ssh-keygen -t rsa -C "ryan.hanks@webfilings.com"
-cat
 ssh-add ~/.ssh/id_rsa
 cd
 
-cat
+cat 
+# wait for confirm 1.
+
 pbcopy < ~/.ssh/id_rsa.pub
 cat ~/.ssh/id_rsa.pub
 say "add the key"
@@ -21,14 +24,113 @@ cat
 
 ssh -T git@github.com
 cd
-cat
 
 echo "**************************************************"
 echo "press ctrl+d when xcode install completes"
 cat
 
-git clone git@github.com:ryanhanks-wf/brony-ci-runner
-cd ~/brony-ci-runner
-sudo gem install bundler
-sudo bundle
-bundle exec soloist
+# git clone git@github.com:ryanhanks-wf/brono-ci-runner
+# cd ~/brono-ci-runner
+# sudo gem install bundler
+# sudo bundle
+# bundle exec soloist
+
+
+
+
+
+date
+mkdir ~/.pip 
+cat > ~/.pip/pip.conf <<EOF
+[global]
+timeout = 15
+index-url = https://ryanhanks-wf:e789c9a2a4@pypi.webfilings.org/simple/
+EOF
+
+
+ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+brew doctor
+brew install node cmake python ant jenkins
+pip install virtualenv
+ 
+ 
+npm install -g grunt-cli cordova phantomjs
+ 
+sudo gem install compass-rgbapng
+
+# install 2
+ 
+mkdir ~/Library/LaunchAgents
+ln -sfv /usr/local/opt/jenkins/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.jenkins.plist
+
+ 
+# update plugin list from advanced tab
+# download cli.jar from 
+java -jar jenkins-cli.jar -s http://localhost:8080/ install-plugin -restart github
+echo "press ctrl+d when jenkins has restarted"
+cat
+cat > brono.job <<EOF 
+<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties>
+    <com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github@1.8">
+      <projectUrl>https://github.com/ryanhanks-wf/brono/</projectUrl>
+    </com.coravy.hudson.plugins.github.GithubProjectProperty>
+  </properties>
+  <scm class="hudson.plugins.git.GitSCM" plugin="git@2.1.0">
+    <configVersion>2</configVersion>
+    <userRemoteConfigs>
+      <hudson.plugins.git.UserRemoteConfig>
+        <url>git+ssh://git@github.com/ryanhanks-wf/brono</url>
+        <credentialsId>cdb26fcd-d89e-4fe7-b920-ccea219edee4</credentialsId>
+      </hudson.plugins.git.UserRemoteConfig>
+    </userRemoteConfigs>
+    <branches>
+      <hudson.plugins.git.BranchSpec>
+        <name>**</name>
+      </hudson.plugins.git.BranchSpec>
+    </branches>
+    <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+    <submoduleCfg class="list"/>
+    <extensions/>
+  </scm>
+  <canRoam>true</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers>
+    <com.cloudbees.jenkins.GitHubPushTrigger plugin="github@1.8">
+      <spec></spec>
+    </com.cloudbees.jenkins.GitHubPushTrigger>
+    <hudson.triggers.SCMTrigger>
+      <spec>* * * * *</spec>
+      <ignorePostCommitHooks>false</ignorePostCommitHooks>
+    </hudson.triggers.SCMTrigger>
+  </triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell>
+      <command>echo &quot;success&quot;
+rm -rf ~/.brono
+npm install
+brono_path=$(pwd)/bin/brono
+test_workspace=$WORKSPACE/test-workspace
+echo $(mkdir $test_workspace)
+pushd $test_workspace
+rm -rf *
+$brono_path init
+cat brono.json
+pwd
+$brono_path install
+</command>
+    </hudson.tasks.Shell>
+  </builders>
+  <publishers/>
+  <buildWrappers/>
+</project>
+EOF 
+cat brono.job | java -jar jenkins-cli.jar -s http://localhost:8080/ create-job brono-install 
