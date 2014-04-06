b86d60dbfa15ff386e1a00140c7fa80b7ce6c769
Ryan Hanks <ryan.hanks@webfilings.com>
2014-04-06 13:59:07 -0700

    update bootstrap for building ci runners

--- bootstrap.sh
+++ bootstrap.sh
@@ -69,7 +69,7 @@ echo "press ctrl+d when jenkins has started"
 cat
 open http://localhost:8080/jnlpJars/jenkins-cli.jar
 
- 
+cat
 # update plugin list from advanced tab
 # download cli.jar from 
 
@@ -121,20 +121,83 @@ cat > brono.job <<EOF
   <builders>
     <hudson.tasks.Shell>
       <command>echo &quot;success&quot;
-export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/bin:\$PATH
 rm -rf ~/.brono
 npm install
-workspace_path=$WORKSPACE/tmp/$BUILD_NUMBER
-mkdir -p $workspace_path
+workspace_path=\$WORKSPACE/tmp/\$BUILD_NUMBER
+mkdir -p \$workspace_path
 cd $workspace_path
-brono_path=$WORKSPACE/bin/brono
-$brono_path init
-$brono_path install
+brono_path=\$WORKSPACE/bin/brono
+\$brono_path init
+\$brono_path install
       </command>
     </hudson.tasks.Shell>
   </builders>
   <publishers/>
   <buildWrappers/>
 </project>
-EOF 
-cat brono.job | java -jar jenkins-cli.jar -s http://localhost:8080/ create-job brono-install 
+EOF
+cat brono.job | java -jar ~/Downloads/jenkins-cli.jar -s http://localhost:8080/ create-job brono-install 
+
+cat > brono.job <<EOF 
+<?xml version='1.0' encoding='UTF-8'?>
+<project>
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties>
+    <com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github@1.8">
+      <projectUrl>https://github.com/ryanhanks-wf/brono/</projectUrl>
+    </com.coravy.hudson.plugins.github.GithubProjectProperty>
+  </properties>
+  <scm class="hudson.plugins.git.GitSCM" plugin="git@2.1.0">
+    <configVersion>2</configVersion>
+    <userRemoteConfigs>
+      <hudson.plugins.git.UserRemoteConfig>
+        <url>git+ssh://git@github.com/ryanhanks-wf/brono</url>
+        <credentialsId>cdb26fcd-d89e-4fe7-b920-ccea219edee4</credentialsId>
+      </hudson.plugins.git.UserRemoteConfig>
+    </userRemoteConfigs>
+    <branches>
+      <hudson.plugins.git.BranchSpec>
+        <name>**</name>
+      </hudson.plugins.git.BranchSpec>
+    </branches>
+    <doGenerateSubmoduleConfigurations>false</doGenerateSubmoduleConfigurations>
+    <submoduleCfg class="list"/>
+    <extensions/>
+  </scm>
+  <canRoam>true</canRoam>
+  <disabled>false</disabled>
+  <blockBuildWhenDownstreamBuilding>false</blockBuildWhenDownstreamBuilding>
+  <blockBuildWhenUpstreamBuilding>false</blockBuildWhenUpstreamBuilding>
+  <triggers>
+    <com.cloudbees.jenkins.GitHubPushTrigger plugin="github@1.8">
+      <spec></spec>
+    </com.cloudbees.jenkins.GitHubPushTrigger>
+    <hudson.triggers.SCMTrigger>
+      <spec>* * * * *</spec>
+      <ignorePostCommitHooks>false</ignorePostCommitHooks>
+    </hudson.triggers.SCMTrigger>
+  </triggers>
+  <concurrentBuild>false</concurrentBuild>
+  <builders>
+    <hudson.tasks.Shell>
+      <command>echo &quot;success&quot;
+export PATH=/usr/local/bin:\$PATH
+npm update
+workspace_path=\$WORKSPACE/tmp/\$BUILD_NUMBER
+mkdir -p \$workspace_path
+cd \$workspace_path
+brono_path=\$WORKSPACE/bin/brono
+\$brono_path init
+\$brono_path install
+node test/test.js
+      </command>
+    </hudson.tasks.Shell>
+  </builders>
+  <publishers/>
+  <buildWrappers/>
+</project>
+EOF
+cat brono.job | java -jar ~/Downloads/jenkins-cli.jar -s http://localhost:8080/ create-job brono-install 
